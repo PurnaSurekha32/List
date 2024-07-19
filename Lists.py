64fcfad4e60b0c16e2bb1685ae47ebfa16a00341
PurnaSurekha32 <purna32surekha@gmail.com>
2024-07-19 19:57:14 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -21,12 +21,6 @@ Type "copyright", "credits" or "license()" for more information.
 >>> 
 >>> #Extend
 >>> 
->>> a.extend["v",3+3]
-Traceback (most recent call last):
-  File "<pyshell#18>", line 1, in <module>
-    a.extend["v",3+3]
-TypeError: 'builtin_function_or_method' object is not subscriptable
->>> 
 >>> a.extend(["v",3+3])
 >>> a
 [1, 2, 3, 'Sai', (1+8j), True, False, (2+6j), 'v', 6]
@@ -34,13 +28,6 @@ TypeError: 'builtin_function_or_method' object is not subscriptable
 >>> 
 >>> 
 >>> #Insert
->>> 
->>> a="[1, 2, 3, 'Sai', (1+8j), True, False, (2+6j), 'v', 6]"
->>> a.insert(2,"Python")
-Traceback (most recent call last):
-  File "<pyshell#28>", line 1, in <module>
-    a.insert(2,"Python")
-AttributeError: 'str' object has no attribute 'insert'
 >>> a=[1, 2, 3, 'Sai', (1+8j), True, False, (2+6j), 'v', 6]
 >>> a.insert(2,"Python")
 >>> a
@@ -49,12 +36,6 @@ AttributeError: 'str' object has no attribute 'insert'
 >>> 
 >>> 
 >>> #Index
->>> 
->>> a.index("2")
-Traceback (most recent call last):
-  File "<pyshell#37>", line 1, in <module>
-    a.index("2")
-ValueError: '2' is not in list
 >>> a.index(2)
 1
 >>> 
@@ -67,11 +48,6 @@ ValueError: '2' is not in list
 >>> 
 >>> 
 >>> #Remove
->>> a.remove("sai")
-Traceback (most recent call last):
-  File "<pyshell#48>", line 1, in <module>
-    a.remove("sai")
-ValueError: list.remove(x): x not in list
 >>> a.remove("Sai")
 >>> a
 [1, 2, 'Python', 3, (1+8j), True, False, (2+6j), 'v', 6]
@@ -96,23 +72,6 @@ ValueError: list.remove(x): x not in list
 >>> 
 >>> 
 >>> #Sort
->>> 
->>> a.sort()
-Traceback (most recent call last):
-  File "<pyshell#70>", line 1, in <module>
-    a.sort()
-TypeError: '<' not supported between instances of 'str' and 'int'
->>> a=[V,P,A,U,K,O,F]
-Traceback (most recent call last):
-  File "<pyshell#71>", line 1, in <module>
-    a=[V,P,A,U,K,O,F]
-NameError: name 'V' is not defined
->>> a=["V","P","A","U","K","O","F"]
->>> s.sort()
-Traceback (most recent call last):
-  File "<pyshell#73>", line 1, in <module>
-    s.sort()
-NameError: name 's' is not defined
 >>> a.sort()
 >>> a
 ['A', 'F', 'K', 'O', 'P', 'U', 'V']
@@ -131,12 +90,7 @@ NameError: name 's' is not defined
 [6, 'v', (2+6j), False, True, (1+8j), 3, 'Python', 1]
 >>> 
 >>> 
->>> 
->>> a.count()
-Traceback (most recent call last):
-  File "<pyshell#90>", line 1, in <module>
-    a.count()
-TypeError: count() takes exactly one argument (0 given)
+>>> #count
 >>> a.count(2)
 0
 >>> a.count(0)
@@ -145,13 +99,6 @@ TypeError: count() takes exactly one argument (0 given)
 >>> 
 >>> 
 >>> #Len
->>> 
->>> a="V"
->>> a=[V]
-Traceback (most recent call last):
-  File "<pyshell#99>", line 1, in <module>
-    a=[V]
-NameError: name 'V' is not defined
 >>> a=["V"]
 >>> len(a)
 1
